226c77859ef7b2f574f43ea2ed0263250fc465b9
storagerepo <deemsysinchr@gmail.com>
2015-07-10 10:53:58 +0530

    Commit for changes.

--- js/jquery-readmore.js
+++
@@ -1,9 +0,0 @@
-$('.SeeMore').click(function(){
-    var $this = $(this);
-    $this.toggleClass('SeeMore');
-    if($this.hasClass('SeeMore')){
-    $this.text('Read More');
-    } else {
-    $this.text('Read Less');
-    }
-});
