cdfcffde47207f8980e3a308587e5d7a0e470db9
mugeshkumarrepo <mugeshkumars@deemsysinc.com>
2015-07-15 18:43:55 +0530

    Angular UI update

--- angular/scripts/loggedIn/loggedInController.js
+++ angular/scripts/loggedIn/loggedInController.js
@@ -9,6 +9,24 @@ loggedinApp.config(['$routeProvider',
             when('/index', {
                 templateUrl: 'course_category.html'
             }).
+            when('/course_category', {
+                templateUrl: 'course_category.html'
+            }).
+            when('/offer_zone', {
+                templateUrl: 'offer_zone.html'
+            }).
+            when('/news_events', {
+                templateUrl: 'news_events.html'
+            }).
+            when('/news_detail', {
+                templateUrl: 'news_detail.html'
+            }).
+            when('/blog', {
+                templateUrl: 'blog.html'
+            }).
+            when('/blog_post', {
+                templateUrl: 'blog_post.html'
+            }).
             when('/profile_edit', {
                 templateUrl: 'profile_edit.html'
             }).
@@ -74,6 +92,51 @@ loggedinApp.config(['$routeProvider',
             }).
             when('/help', {
                 templateUrl: 'help.html'
+            }).
+            when('/my_wishlist', {
+                templateUrl: 'my_wishlist.html'
+            }).
+            when('/my_students', {
+                templateUrl: 'my_students.html'
+            }).
+            when('/my_favourites', {
+                templateUrl: 'my_favourites.html'
+            }).
+            when('/my_messages', {
+                templateUrl: 'my_messages.html'
+            }).
+            when('/my_messages_details', {
+                templateUrl: 'my_messages_details.html'
+            }).
+            when('/my_purchase_history', {
+                templateUrl: 'my_purchase_history.html'
+            }).
+            when('/my_payment', {
+                templateUrl: 'my_payment.html'
+            }).
+            when('/term_of_use', {
+                templateUrl: 'term_of_use.html'
+            }).
+            when('/privacy_policy', {
+                templateUrl: 'privacy_policy.html'
+            }).
+            when('/refund_policy', {
+                templateUrl: 'refund_policy.html'
+            }).
+            when('/affiliate_login', {
+                templateUrl: 'affiliate_login.html'
+            }).
+            when('/about_us', {
+                templateUrl: 'about_us.html'
+            }).
+            when('/promocode', {
+                templateUrl: 'promocode.html'
+            }).
+            when('/FAQ', {
+                templateUrl: 'FAQ.html'
+            }).
+            when('/contacts', {
+                templateUrl: 'contacts.html'
             }).
             otherwise({
                 redirectTo: '/index'
